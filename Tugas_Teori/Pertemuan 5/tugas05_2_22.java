a0fd5d38fb5d8300501327647313fecdbf29fc0a
dyodrann <addhabiburrahman@gmail.com>
2026-09-23 09:10:09 +0700

    Add files via upload

---
+++ Tugas_Teori/Pertemuan 5/tugas05_2_22.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class tugas05_2_22 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int layanan;
+        System.out.println("Pilih layanan :");
+        layanan = scanner.nextInt();
+        
+        switch (layanan) {
+            case 1:
+                System.out.println("Layanan legalisir ijazah di Loket A");
+                break;
+            case 2:
+                System.out.println("Layanan Keterangan Aktif Kuliah di Loket B");
+                break;
+            case 3:
+                System.out.println("Layanan Pembayaran UKT di Loket C");
+                break;
+            case 4 :
+                System.out.println("Layanan Pengajuan Cuti Akademik di Loket D");
+                break;    
+            default:
+                System.out.println("Layanan tidak tersedia");
+                break;
+                
+        }
+        
+
+    }
+}
